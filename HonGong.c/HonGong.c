40372217170d6f2b0abf67f48d1fe99bd9e1c8de
soi <010127js@gmail.com>
2023-08-25 10:04:37 +0900

    초기 커밋.

---
+++ HonGong.c/HonGong.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", 10);
+	printf("%lf\n", 3.4);
+	printf("%.1lf\n", 3.45);
+	printf(" % .10lf\n",3.4);
+
+	printf("%d과 %d의 합은 %d입니다\n", 10, 20, 10 + 20);
+	printf("%.1lf-%.1lf = %.1lf\n", 3.4, 1.2, 3.4 - 1.2);
+
+}
